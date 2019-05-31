bba12f048af28351bc66571eeff7c767b7e336b0
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-05-31 07:00:29 +0000

    top_controllerのindex、createメソッドを投稿した内容を表示するため編集

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,11 +1,17 @@
 class TopController < ApplicationController
   def index
+    @articles = Article.all 
   end
   
   def new
   end
   
   def create
+    Article.create(text: article_params[:text], user_id: current_user.id)
   end
   
+  private
+  def article_params
+    params.permit(:text)
+  end
 end
