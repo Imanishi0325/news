29655243855c47b2b4c0ed09d071b40e3cf2bf12
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-03 23:40:41 +0000

    6c2371eの作業を修正し、ログインしているユーザーidを取得して作成日順に並べるメソッドを追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @articles = Article.where(user_id: current_user.id).page(params[:page]).per(10).order("created_at DESC")
   end
 end
