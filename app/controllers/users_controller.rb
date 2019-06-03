380ddc9b2f010a2ad9d4ec3b8bc6df2dc56fd966
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-03 23:51:02 +0000

    users_controllerにログイン中ユーザーのニックネームを取得するメソッドを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @articles = Article.where(user_id: current_user.id).page(params[:page]).per(10).order("created_at DESC")
+    @nickname = current_user.nickname
   end
 end
