70f3e80f13a2fcd1281467585a365882fb9b53e5
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-04 07:14:48 +0000

    マイページ上で記事を削除できるようルーティングを追加設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#index"
   resources :top, only: [:index, :new, :create]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :destroy]
 end
