894d89c4d957c938378a926a1cae847f9775b479
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-04 07:24:21 +0000

    users_controllerにdestroyアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @articles = Article.where(user_id: current_user.id).page(params[:page]).per(10).order("created_at DESC")
     @nickname = current_user.nickname
   end
+  
+  def destroy
+  end
+  
 end
