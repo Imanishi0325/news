bba41e1f5183a37c9aba235c8171b074f276fd3c
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-05 00:24:28 +0000

    編集機能追加のためルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root "top#index"
-  resources :top, only: [:index, :new, :create, :destroy]
+  resources :top, only: [:index, :new, :create, :destroy, :edit]
   resources :users, only: [:show]
 end
