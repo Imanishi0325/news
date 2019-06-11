cb4d8dc594e77cecef83f13572fc4af07eaeb76e
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-11 09:13:05 +0000

    user_paramsにimageを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.permit(:email, :nickname, :password) 
+    params.permit(:email, :nickname, :password, :image) 
   end
 end
