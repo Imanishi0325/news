d11621c1bcf59632fcc9f5d2ef6c0ddfdf9fb7c5
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-18 07:24:12 +0000

    migrationファイルの中身を修正

--- db/migrate/20190618071813_create_comments.rb
+++ db/migrate/20190618071813_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.integer :user.id
-      t.integer :article.id
+      t.integer :user_id
+      t.integer :article_id
       t.text :text
       t.timestamps
     end
