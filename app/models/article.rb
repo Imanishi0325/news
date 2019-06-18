7e0aeb8182cec4e024cfb1665048e31192e204af
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-18 07:32:10 +0000

    アソシエーション組成のため各モデルファイルを編集

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :text, presence: true
   validates :text, length: { in: 1..500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :articles
+         has_many :comments
          has_one_attached :image
          validates :nickname, presence: true
 end
