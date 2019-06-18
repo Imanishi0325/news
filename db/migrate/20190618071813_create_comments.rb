a1fe0ea6199dde6c4cfb5e118ce8c0da2e69fe34
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-18 07:22:37 +0000

    migrationファイルにカラムを追加

--- db/migrate/20190618071813_create_comments.rb
+++ db/migrate/20190618071813_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.integer :user.id
+      t.integer :article.id
+      t.text :text
       t.timestamps
     end
   end
