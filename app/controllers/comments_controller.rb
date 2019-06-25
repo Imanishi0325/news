bcc336fd61cfcfa1075a1fb75dc60e2f34b8ce8f
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-25 05:43:26 +0000

    comments_controllersのredirect処理をajaxのため削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(text: comment_params[:text], article_id: comment_params[:top_id], user_id: current_user.id)
-    redirect_to "/top/#{@comment.article.id}"
   end
   
   def destroy
