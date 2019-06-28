e63c60e583d90e2a766d406272149c12320e294b
EC2 Default User <ec2-user@ip-172-31-24-221.ap-southeast-1.compute.internal>
2019-06-28 14:58:50 +0000

    ユーザーページの記事毎のコメントの表示を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @articles = Article.where(user_id: current_user.id).page(params[:page]).per(10).order("created_at DESC")
     @nickname = current_user.nickname
-    @user = User.find(params[:id])  
+    @user = User.find(params[:id])
     @comments = Comment.where(params[:id])
   end
   
